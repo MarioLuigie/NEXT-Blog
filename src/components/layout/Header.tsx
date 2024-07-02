4e2fcd6fa1360ce652b5fd2be016bfa0579bda64
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-07-03 01:50:09 +0200

    Refactor

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,3 +28,36 @@ export default async function Header() {
 		</header>
 	)
 }
+
+// import Link from 'next/link';
+// import Logo from '@/components/content/Logo';
+// import Nav from '@/components/layout/Nav';
+// import { paths } from '@/lib/constants/paths';
+// import { RegisterLink, LoginLink, LogoutLink, useKindeAuth } from '@kinde-oss/kinde-auth-nextjs/components';
+// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+// import { toastSuccess } from '@/lib/utils/toasts';
+
+
+// export default async function Header() {
+//   const { isAuthenticated, logout } = useKindeAuth();
+  
+//   const handleLogout = async () => {
+//     await logout();
+//     toastSuccess({ message: 'Logged out successfully' });
+//   };
+
+//   return (
+//     <header className="bg-zinc-100 shadow-lg p-8 z-40 flex-between h-[140px] sticky top-0 left-0 w-full">
+//       <Link href={paths.HOME}>
+//         <Logo />
+//       </Link>
+//       <Nav />
+//       {!(await isAuthenticated()) ? (
+//         <LoginLink>Sign in</LoginLink>
+//       ) : (
+//         <button onClick={handleLogout}>Log out</button>
+//       )}
+//     </header>
+//   );
+// }
+
