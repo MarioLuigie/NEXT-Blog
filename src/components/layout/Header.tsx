40c038ff1050028e3bcb789c2221d31370c7ed18
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-07-03 01:38:07 +0200

    Feat: Add Logout and SignIn to Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,14 +5,26 @@ import Logo from '@/components/content/Logo'
 import Nav from '@/components/layout/Nav'
 //lib
 import { paths } from '@/lib/constants/paths'
+import {
+	RegisterLink,
+	LoginLink,
+	LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export default function Header() {
+export default async function Header() {
+	const { isAuthenticated } = getKindeServerSession()
 	return (
 		<header className="bg-zinc-100 shadow-lg p-8 z-40 flex-between h-[140px] sticky top-0 left-0 w-full">
 			<Link href={paths.HOME}>
 				<Logo />
 			</Link>
 			<Nav />
+			{!(await isAuthenticated()) ? (
+				<LoginLink>Sign in</LoginLink>
+			) : (
+				<LogoutLink>Log out</LogoutLink>
+			)}
 		</header>
 	)
 }
