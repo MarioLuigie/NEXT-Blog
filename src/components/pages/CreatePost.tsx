b3a248555652bfc917e5bab3384eeb52ade2b0b2
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-07-03 01:22:24 +0200

    Feat: Add kinde auth

--- src/components/pages/CreatePost.tsx
+++ src/components/pages/CreatePost.tsx
@@ -1,12 +1,21 @@
 import CreatePostForm from '@/components/forms/CreatePostForm'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
 export default async function CreatePost() {
-	return (
-		<div className="grow flex-center">
-			<div className="p-10 rounded-md shadow-xl flex flex-col gap-10 max-w-[600px] min-w-[400px]">
-				<p className="text-zinc-600 font-medium text-2xl">Create Post</p>
-				<CreatePostForm />
+	const { isAuthenticated } = getKindeServerSession()
+
+	if(await isAuthenticated()) {
+		return (
+			<div className="grow flex-center">
+				<div className="p-10 rounded-md shadow-xl flex flex-col gap-10 max-w-[600px] min-w-[400px]">
+					<p className="text-zinc-600 font-medium text-2xl">Create Post</p>
+					<CreatePostForm />
+				</div>
 			</div>
-		</div>
-	)
+		)
+	} else {
+		redirect('/api/auth/login?post-login-redirect-url=/create-post')
+	}
+
 }
